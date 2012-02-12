e05a8766e0e8b2c2fe0cda1390d1ab2ea6ef0d8a
Anthony White <awhite@dataxu.com>
2012-02-12 18:58:26 -0500

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Houses::Application.config.secret_token = '7c105b8b8e2731e97e0ab533949390b1ace5a2264e2745ff0efbd4857b1deea3c5574f5336b7ed012a9cddf1588ef6357fe5a78a878b89357a13f9cf02d3871b'
