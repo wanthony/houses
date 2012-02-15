9226787b5859ecb837f5dd18b4fcf55e725aeaff
Anthony White <anthony.white.j@gmail.com>
2012-02-14 21:25:20 -0500

    Clean up ApplicationHelper
    Update form layout helpers
    Clean up how different divs are handled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,36 @@
 module ApplicationHelper
-  def control_group
-    content_tag :div, :class => 'control-group' do
+  def div(options = {})
+    options.reverse_merge!(:class => [])
+    options[:class] = [options[:class]] unless options[:class].is_a?(Array)
+    content_tag :div, :class => options[:class].join(' ') do
       yield
     end
   end
 
-  def controls
-    content_tag :div, :class => 'controls' do
+  def control_group(options = {})
+    options[:class] = ['control-group', options[:class]]
+    div options do
+      yield
+    end
+  end
+
+  def controls(options = {})
+    options[:class] = ['controls', options[:class]]
+    div options do
+      yield
+    end
+  end
+
+  def columns(width = 12, options = {})
+    options[:class] = ["span#{width}", options[:class]]
+    div options do
+      yield
+    end
+  end
+
+  def row(options = {})
+    options[:class] = ["row", options[:class]]
+    div options do
       yield
     end
   end
